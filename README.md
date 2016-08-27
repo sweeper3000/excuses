01a9a87840fc6f8bf0c30d33bda188ff8b05f945
sweeper3000 <spiketwofish@protonmail.com>
2016-08-27 17:05:59 -0400

    Edit readme and push script

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ excuses requires python 3. Will not work with python 2.
 
 ## Usage
 From a terminal, run `python3 excuses.py` in the containing directory.
+
+## Suggest new excuses
+- Open a new issue
+- The excuse should be as universal as possible. I reserve the right to edit/reject the excuse. Crazy or unbelievable excuses will not be considered.
---
+++ excuses.py
@@ -0,0 +1,33 @@
+import random
+
+# List excuses
+excuses = [
+	'I don\'t feel very well',
+	'I have lots of homework',
+	'My girlfriend/boyfriend and I have plans',
+	'I have to go to a relative\'s birthday',
+	'I have to babysit my sibling(s)',
+	'My family is going out for breakfast/lunch/dinner',
+	'I\'m expecting someone at that time',
+	'I have to go to a wedding',
+	'I have to do community service',
+	'I have to help my mother/father with their computer',
+	'I\'m going far away from here to escape the Mormons',
+	'I have to help clean the house',
+	'My family and I are going on a vacation',
+]
+
+"""
+ _________________________________________
+/ Remember no need to run faster than the \
+\ bear just the guy in front of you       /
+ -----------------------------------------
+ \
+  \
+   \ >()_
+      (__)__ _
+
+"""
+
+# print random excuse from list
+print(random.choice(excuses))
